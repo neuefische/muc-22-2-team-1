5d54901b7b6abb15a43bab9d253b5dd38e535ed1
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2022-11-25 12:44:21 +0100

    Add WizardController

--- backend/src/main/java/de/neuefische/backend/controller/WizardController.java
+++ backend/src/main/java/de/neuefische/backend/controller/WizardController.java
@@ -1,4 +1,10 @@
 package de.neuefische.backend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/WizardStudents")
 public class WizardController {
+
 }
