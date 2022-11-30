a6ad4cfa5f0ea5a016ebd249109cf89ead7341d3
martinkascak <m.kascak93@gmail.com>
2022-11-30 12:46:06 +0100

    Add controller

--- backend/src/main/java/de/neuefische/backend/controller/WizardController.java
+++ backend/src/main/java/de/neuefische/backend/controller/WizardController.java
@@ -1,10 +1,63 @@
 package de.neuefische.backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.neuefische.backend.model.Wizard;
+import de.neuefische.backend.service.WizardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/wizard-students")
 public class WizardController {
 
+    private  final WizardService wizardService;
+
+    @Autowired
+    public WizardController(WizardService wizardService) {
+        this.wizardService = wizardService;
+    }
+
+    @GetMapping
+    public List<Wizard> listWizards(@RequestParam Optional<String> search) {
+        if (search.isPresent()) {
+            return wizardService.search(search.get());
+        }
+        return wizardService.list();
+    }
+
+    @GetMapping("{id}")
+    public Wizard getWizard(@PathVariable String id) throws IllegalAccessException {
+        return wizardService.findById(id);
+    }
+
+    @PostMapping()
+    public Wizard addWizard(@RequestBody Wizard wizard){
+        return wizardService.addWizard(wizard);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteWizard(@PathVariable String id) throws IllegalAccessException {
+        wizardService.delete(id);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- backend/src/main/java/de/neuefische/backend/model/Wizard.java
+++ backend/src/main/java/de/neuefische/backend/model/Wizard.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Objects;
 
 @With
-@Document("studentDB")
+@Document("wizardsDB")
 public record Wizard(
         @Id
         String id,
