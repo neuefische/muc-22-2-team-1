d0e4279f95647084f7e743773ad7805a17f08216
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2022-11-30 17:38:29 +0100

    Add WizardApp.tsx Add useWizards.ts

---
+++ frontend/src/components/WizardApp.tsx
@@ -0,0 +1,17 @@
+import useWizards from "../hooks/useWizards";
+import AddWizard from "./AddWizard";
+import WizardGallery from "./WizardGallery";
+
+export default function WizardApp() {
+
+    const { wizards, addWizard, removeWizard} = useWizards()
+
+    return(
+        <div>
+            <h1>Wizards</h1>
+            <h2>Die ZaubererDatenbank</h2>
+            <AddWizard addWizard={addWizard}/>
+            <WizardGallery wizards={wizards} removeWizard={removeWizard}/>
+        </div>
+    )
+}
---
+++ frontend/src/hooks/useWizards.ts
@@ -0,0 +1,39 @@
+import {NewWizard, Wizard} from "../model/Wizard";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export default function useWizards(){
+
+    const [wizards, setWizards] = useState<Wizard[]>([])
+
+    useEffect( ()=> {
+        getWizards()
+    }, [])
+
+    function getWizards(){
+        axios.get("/wizard-students")
+            .then(response =>{
+                setWizards(response.data)
+            })
+            .catch(console.error)
+    }
+
+    function addWizard(newWizard: NewWizard) {
+        return axios.post("wizard-students", newWizard)
+            .then(response => response.data)
+            .then((savedWizard) => setWizards(prevState => [...prevState,savedWizard]))
+            .catch(console.error)
+    }
+
+    function removeWizard(id:string){
+        axios.delete(`/wizard-students/${id}`)
+            .then(()=> {
+                setWizards(prevState => {
+                    return prevState.filter((wizard) => wizard.id ! == id
+                    )
+                })
+            })
+    }
+
+    return{wizards, addWizard, removeWizard}
+}
