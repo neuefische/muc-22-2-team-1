40f48968466cc9a92d453aba3b044b336a714f3e
licetullmann <licetrosales@gmail.com>
2022-12-01 17:42:36 +0100

    Add NavigationBar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
 import WizardApp from './components/WizardApp';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NavigationBar from "./components/NavigationBar";
+
 
 function App() {
-  return (
-    <div className="App">
-      <div><WizardApp></WizardApp></div>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <NavigationBar/>
+
+                <Routes>
+                    <Route path={""} element={<WizardApp/>}> </Route>
+                    <Route path={"/Startseite"} element={<p>Wizard Welt</p>}> </Route>
+                    <Route path={"/wizards"} element={<div>Galery</div>}> </Route>
+                </Routes>
+
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
