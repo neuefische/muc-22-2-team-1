2274da5c9db69354f2301b3fc1ec13c709aac6cb
licetullmann <licetrosales@gmail.com>
2022-12-05 12:55:48 +0100

    Add login for MongoUser

---
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package de.neuefische.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeRequests()
+                .antMatchers("/wizard-students").authenticated()
+                .antMatchers("/**").permitAll()
+                .and()
+                .build();
+    }
+}
