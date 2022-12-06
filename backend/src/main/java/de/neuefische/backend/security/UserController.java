ec4aae47446820ec67af50cf5cfdf663225d76f4
licetullmann <licetrosales@gmail.com>
2022-12-06 15:33:08 +0100

    Add new login and logout

--- backend/src/main/java/de/neuefische/backend/security/UserController.java
+++ backend/src/main/java/de/neuefische/backend/security/UserController.java
@@ -1,17 +1,38 @@
 package de.neuefische.backend.security;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("api/users")
 public class UserController {
 
     @GetMapping("me") //Spring injected ein Principle Objekt in dem der aktuell eingeloggt User steckt
     public String helloMe(Principal principal) {
-        return "Hallo " + principal.getName();
+        if (principal != null) {
+            return principal.getName();
+        }
+        return "anonymousUser";
+    }
+
+    @PostMapping("login")
+    public String login() {
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+    }
+
+    @PostMapping("logout")
+    public String logout(HttpSession httpSession) {
+        httpSession.invalidate();
+        SecurityContextHolder.clearContext();
+        return "anonymousUser";
     }
 }
---
+++ frontend/src/components/LoginPage.tsx
@@ -0,0 +1,44 @@
+import {Button, TextField} from "@mui/material"
+import {ChangeEvent, FormEvent, useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+type LoginPageProps = {
+    login: (username: string, password: string) => Promise<string>
+}
+
+
+export default function LoginPage(props: LoginPageProps) {
+
+    const [username, setUsername] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
+    const navigate = useNavigate()
+
+    function onUsernameChange(event: ChangeEvent<HTMLInputElement>) {
+        setUsername(event.target.value)
+    }
+
+    function onPasswordChange(event: ChangeEvent<HTMLInputElement>) {
+        setPassword(event.target.value)
+    }
+
+    function onLoginSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        props.login(username, password)
+            .then(user => {
+                navigate("/students")
+            })
+    }
+
+    return (
+        <div>
+            <h2>Login Page</h2>
+            <form onSubmit={onLoginSubmit}>
+                <TextField placeholder={"Username"} value={username} onChange={onUsernameChange}/>
+                <TextField type={"password"} placeholder={"Password"} value={password} onChange={onPasswordChange}/>
+                <Button type={"submit"}>Login</Button>
+            </form>
+        </div>
+    )
+}
