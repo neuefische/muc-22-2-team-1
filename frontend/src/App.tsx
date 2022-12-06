ceb736010351ad4e341a4fd8a99b3e278088546c
licetullmann <licetrosales@gmail.com>
2022-12-06 15:33:08 +0100

    Add new login and logout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,26 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
 import WizardGalleryApp from "./components/WizardGalleryApp";
+import LoginPage from "./components/LoginPage";
+import useUser from "./hooks/useUser";
 
 
 function App() {
+
+    const {username, login, logout} = useUser();
+
   return (
     <div className="App">
       <div>
           <BrowserRouter>
-              <NavigationBar/>
+              <NavigationBar logout={logout}/>
+              <h2>Hallo {username}!</h2>
 
               <Routes>
                   <Route path={""} element={<Home/>}> </Route>
                   <Route path={"/speaking-hat"} element={<WizardApp/>}> </Route>
                   <Route path={"/wizard-gallery"} element={<WizardGalleryApp/>}> </Route>
+                  <Route path={"/login"} element={<LoginPage login={login}/>} />
               </Routes>
 
           </BrowserRouter>
--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,12 +1,18 @@
+import { Button } from "@mui/material";
 import {NavLink} from "react-router-dom";
 
-export default function NavigationBar(){
+type NavigationBarProps = {
+    logout: () => Promise<string>
+}
+export default function NavigationBar(props: NavigationBarProps){
 
     return (
         <div>
             <NavLink to={"/"}>Home</NavLink>
             <NavLink to={"/speaking-hat"}>Speaking Hat</NavLink>
             <NavLink to={"/wizard-gallery"}>Wizard Gallery</NavLink>
+            <NavLink to={"/login"} >Login</NavLink>
+            <Button onClick={props.logout}>Logout</Button>
         </div>
     )
 }
