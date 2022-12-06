43ed5e5f190ec6f147733f00769140bf8d488620
licetullmann <licetrosales@gmail.com>
2022-12-06 15:53:58 +0100

    Add ProtectedRoutes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,31 +7,38 @@ import Home from "./components/Home";
 import WizardGalleryApp from "./components/WizardGalleryApp";
 import LoginPage from "./components/LoginPage";
 import useUser from "./hooks/useUser";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 
 function App() {
 
     const {username, login, logout} = useUser();
 
-  return (
-    <div className="App">
-      <div>
-          <BrowserRouter>
-              <NavigationBar logout={logout}/>
-              <h2>Hallo {username}!</h2>
+    return (
+        <div className="App">
+            <div>
+                <BrowserRouter>
+                    <NavigationBar logout={logout}/>
+                    <h2>Hallo {username}!</h2>
 
-              <Routes>
-                  <Route path={""} element={<Home/>}> </Route>
-                  <Route path={"/speaking-hat"} element={<WizardApp/>}> </Route>
-                  <Route path={"/wizard-gallery"} element={<WizardGalleryApp/>}> </Route>
-                  <Route path={"/login"} element={<LoginPage login={login}/>} />
-              </Routes>
+                    <Routes>
+                        <Route path={""} element={<Home/>}> </Route>
+                        <Route path={"/login"} element={<LoginPage login={login}/>}/>
 
-          </BrowserRouter>
+                        <Route element={
+                            <ProtectedRoutes username={username}/>
+                        }>
+                            {/* Alles hier drin ist protected */}
+                            <Route path={"/speaking-hat"} element={<WizardApp/>}> </Route>
+                            <Route path={"/wizard-gallery"} element={<WizardGalleryApp/>}> </Route>
+                        </Route>
+                    </Routes>
 
-      </div>
-    </div>
-    );
+                </BrowserRouter>
+
+            </div>
+        </div>
+);
 }
 
 export default App;
