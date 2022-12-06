4886442dd90f294939b65b86a32cd089e87c8ad1
licetullmann <licetrosales@gmail.com>
2022-12-06 09:32:05 +0100

    Move search feld in Gallery

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import WizardApp from './components/WizardApp';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
+import WizardGalleryApp from "./components/WizardGalleryApp";
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
               <Routes>
                   <Route path={""} element={<Home/>}> </Route>
                   <Route path={"/speaking-hat"} element={<WizardApp/>}> </Route>
-                  <Route path={"/wizard-gallery"} element={<div>Wizard Gallery</div>}> </Route>
+                  <Route path={"/wizard-gallery"} element={<WizardGalleryApp/>}> </Route>
               </Routes>
 
           </BrowserRouter>
